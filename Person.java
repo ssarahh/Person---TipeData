0361ecc5a0c5c8513482a37b97e8d18aa18763b0
ssarahh <sitisarahsuryaningtyas@gmail.com>
2013-09-20 10:51:47 +0700

    Person.java

---
+++ Person.java
@@ -0,0 +1,11 @@
+class Person
+{
+  public static void main(String[] abc) 
+  {
+  int x = 10;
+  char y;	
+  y = 'A';
+  System.out.println("menampilkan variabel x = "+x);
+  System.out.println("menampilkan variabel y = "+y);
+}
+}
